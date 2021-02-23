f7da14559038871d72d11bfa1b745ef28c619833
Prithvi Devaiah <prithvidevaiah.gaming@gmail.com>
2021-02-23 23:50:48 +0530

    Changed the app.py to include headless chrome and accessing them using selenium.

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-while(True):
-    print("Hello")
+from selenium import webdriver
+import os
+
+op = webdriver.ChromeOptions()
+op.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+op.add_argument("--headless")
+op.add_argument("--no-sandbox")
+op.add_argument("--diable-dev-sh-usage")
+
+driver = webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options = op)
+
+driver.get("https://youtube.com")
+print(driver.page_source)
